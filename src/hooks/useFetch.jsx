fb20971177806a901c668c946a6014bccd3b2a33
Matheus Luz <81210255+MathLxz@users.noreply.github.com>
2025-04-29 16:49:31 -0300

    Update useFetch.jsx
    fix typo in hook name

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-export const useFetech = (url) => {
+export const useFetch = (url) => {
   const [data, setData] = useState(null);
 
   const [config, setConfig] = useState(null);
